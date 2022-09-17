96ee2690fee287a2d751473439e412c8b470407e
Luiz Fernando Barbosa <71241157+luizfbarbosa12@users.noreply.github.com>
2022-09-17 20:18:21 -0300

    day 5

--- nlw-esports/web/src/components/Form/Input.tsx
+++ nlw-esports/web/src/components/Form/Input.tsx
@@ -8,7 +8,7 @@ const Input = (props: InputProps) => {
     <input
      {...props}
       className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500"
-      type="text"
+      type={props.type}
      
     />
   );
